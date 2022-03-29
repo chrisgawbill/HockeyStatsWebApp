9f0c91aeb7e38d498bfc58000381913a1780332b
= <cgawbill@protonmail.com>
2022-03-28 23:39:22 -0400

    Pulled in team list through api, started creating side panels that will hold team and player quick look data

---
+++ src/public/home.js
@@ -0,0 +1,107 @@
+window.onload = () => {
+    displayList();
+}
+let displayList = () =>{
+    $.ajax({url:'/GetAllTeams', success:function(res){
+        if(!(res === 'error')){
+            const display = document.getElementById('home-team-list-div');
+            const list = document.createElement("ul");
+            list.className = "home-team-list";
+
+            let name = null;
+            let id = null;
+
+            for(let i = 0; i < res.length; i++){
+                let returnValue = res[i].split("|");
+                name = returnValue[0];
+                id = returnValue[1];
+                console.log(name);
+
+                const listItem = document.createElement("li");
+                const listItemText = document.createTextNode(name);
+
+                listItem.appendChild(listItemText);
+                listItem.className = "team-" + id;
+                listItem.id = name;
+
+                listItem.addEventListener('click', function(e){
+                    let passingTeamName = e.target.id;
+                    let passingTeamID = e.target.className;
+                    displayTeamInfo(passingTeamName, passingTeamID);
+                });
+
+                list.appendChild(listItem);
+            }
+            display.appendChild(list);
+        }else{
+            console.log("error");
+        }
+    }});
+}
+let displayTeamInfo = (teamName, teamID) =>{
+    let teamListDiv = document.getElementById("home-team-list-div");
+    let teamDiv = document.getElementById("team-div");
+
+    teamListDiv.style.width = '60%';
+    teamListDiv.style.height = '500px';
+    teamDiv.style.width = '35%';
+    teamDiv.style.height = '500px';
+
+    teamDiv.style.border = 'solid';
+
+    teamListDiv.style.float = 'left';
+    teamDiv.style.float = 'right';
+
+    wipeTeamPanel();
+    fillTeamPanel(teamName, teamID);
+
+}
+let fillTeamPanel = (teamName, teamID) =>{
+    let teamDiv = document.getElementById("team-div");
+    
+    let teamHeader = document.createElement('h2');
+    let teamHeaderText = document.createTextNode(teamName);
+
+    let playerTestHead = document.createElement("p");
+    let playerTestHeadText  = document.createTextNode("Test Player Name");
+
+
+    teamHeader.appendChild(teamHeaderText);
+    playerTestHead.appendChild(playerTestHeadText);
+
+    playerTestHead.addEventListener('click', fillPlayerPanel);
+
+    teamDiv.appendChild(teamHeader);
+    teamDiv.appendChild(playerTestHead);
+}
+let fillPlayerPanel = () => {
+    let teamDiv = document.getElementById("team-div");
+
+    let playerDiv = document.createElement('div');
+    playerDiv.style.borderLeft = 'none';
+    playerDiv.style.borderRight = 'none';
+    playerDiv.style.borderTop = 'solid';
+    playerDiv.style.borderBottom = 'solid';
+    playerDiv.style.height = '50%';
+
+    let playerHead = document.createElement('h2');
+    let playerTextHead =  document.createTextNode("Claude Giroux");
+
+    playerHead.appendChild(playerTextHead);
+
+    playerDiv.appendChild(playerHead);
+    teamDiv.appendChild(playerDiv);
+
+}
+let wipeTeamPanel = () => {
+    let display = document.getElementById("team-div");
+    while (display.hasChildNodes()) {
+        display.removeChild(display.firstChild);
+    }
+}
+let wipePlayerPanel = () =>{
+    let display = document.getElementById("player-div");
+    while (display.hasChildNodes()) {
+        display.removeChild(display.firstChild);
+    }
+}
--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/home.html');
+    res.sendFile(__dirname + '/public/home.html');
 });
 app.get('/Home', function(req, res){
-    res.sendFile(__dirname + '/home.html');
+    res.sendFile(__dirname + '/public/home.html');
 });
 app.get('/GetAllTeams', function(req, res){
     axios.get(
@@ -18,8 +18,37 @@ app.get('/GetAllTeams', function(req, res){
           
               // Print data
               .then(response => {
-                 console.log(response.data);
-                 res.send(response.data);
+                 let returnValueArray = new Array();
+                 for(let i = 0; i < response.data.teams.length; i++){
+                     let returnValue = response.data.teams[i].name + "|" + response.data.teams[i].id;
+                     returnValueArray.push(returnValue);
+                 }
+                 returnValueArray.sort();
+                 res.send(returnValueArray);
+              })
+          
+              // Print error message if occur
+              .catch(error => res.send('error'))
+});
+app.get('/GetTeamInfo/:teamID', function(req, res){
+    axios.get(
+        `https://statsapi.web.nhl.com/api/v1/teams/` + req.params.teamID)
+          
+              // Print data
+              .then(response => {
+                console.log(response.data);
+              })
+          
+              // Print error message if occur
+              .catch(error => res.send('error'))
+});
+app.get('/GetTeamStats/:teamID', function(req, res){
+    axios.get(
+        `https://statsapi.web.nhl.com/api/v1/teams/` + req.params.teamID + "/stats")
+          
+              // Print data
+              .then(response => {
+                console.log(response.data);
               })
           
               // Print error message if occur
