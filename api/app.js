8280f09d32daa3fbfc9bbf944f8814b48eaf0f4e
Chris Gawbill <cgawbill@protonmail.com>
2026-04-29 15:35:30 -0600

    updating api

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ var scheduleRouter = require("./routes/schedule");
 const { spawn } = require("child_process");
 
 let corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:5173"],
+  origin: ["http://localhost:3000", "http://localhost:5173", "https://chrisgawbill.github.io"],
 };
 
 var app = express();
--- react/src/Services/AxiosInstance.ts
+++ react/src/Services/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosExpressHandler = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:9000",
   headers: {'Access-Control-Allow-Headers ' : '*'},
   withCredentials: false
 });
